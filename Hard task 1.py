f842499a43ae76da726214d26db8ccc05b231bcf
kr1chabtw <149838559+kr1chabtw@users.noreply.github.com>
2024-10-10 02:00:49 +0500

    Hard task 1.py

--- Hard task 1.py
+++ Hard task 1.py
@@ -7,7 +7,7 @@ print(array)
 array.reverse()
 for j in range(1, len(array)): # без 0 индекса, так как у него нету левого соседа
     if array[j-1] < array[j] > array[j+1]:
-        print(array[j])
+        print(f'Индеĸс лоĸального маĸсимума - {len(array) - j - 1}') # -1 из-за 0 индекса, который не идет в учет
         break
     else:
         continue
